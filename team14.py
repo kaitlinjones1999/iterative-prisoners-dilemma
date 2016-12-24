d034ca4cdeeaa37e9ab43629c95045dab8c75b32
kaitlinjones1999 <kaitlin.jones@smackover.net>
2016-12-23 19:09:07 -0600

    Strategy
    The strategy for team 14

--- team14.py
+++ team14.py
@@ -8,7 +8,7 @@
 
 team_name = 'Team Fourteen' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_description = 'This teams strategy is too collude'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
